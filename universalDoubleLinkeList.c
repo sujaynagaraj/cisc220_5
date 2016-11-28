0e1b4ea06b2c90a481b7eda5bd7e59da16ecc45d
sujaynagaraj <sujay.ngrj@gmail.com>
2016-11-28 10:12:18 -0500

    Create universalDoubleLinkeList.c

---
+++ universalDoubleLinkeList.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+Union Node {
+  int intData;
+  int* inPointData
+  float floatData;
+  float* floatPointData;
+  char charData;
+  char* charPointData;
+  
+  int index;
+  
+  Union Node* previous;
+  Union Node* next;
+}
+
+void insert(int index, union Data data){
+
+}
+
+int main(){
+
+return(0);
+}
