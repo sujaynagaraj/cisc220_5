9739f951b5b7f4226a70b2f092c756c69ea9a64b
sujay <sujay.ngrj@gmail.com>
2016-11-28 20:48:46 -0500

    untested

--- universalDoubleLinkeList.c
+++ universalDoubleLinkeList.c
@@ -24,14 +24,59 @@ Node* head = NULL;
 Node* last = NULL;
 Node* current=NULL;
 void insert(int index, union Data data){
-  Node* current=head;
+	int i;
+	Node* newNode, *temp;
+	if (head==NULL){
+	}
+	else {
+		temp=head;
+		i=1;
+		while(i<index-1 && temp!=NULL){ // traversing through list from top
+			temp=temp->next;
+			i++
+
+		}
+		newNode= (Node*)malloc(sizeof(Node)); //allocate memory
+		newNode->Data=data;// put data into newNode
+		newNode->next=temp->next;
+		newNode->prev=temp;
+		if (temp->next != NULL){
+			temp->next->prev=newNode;
+		}
+		temp->next=newNode;
+	}
   
-  Node* new_node =(Node*) malloc(sizeof(Node)); // allocate memory for new_node
   
   
 }
 
+void displayList()
+{
+    Node * temp;
+    int n = 1;
+ 
+    if(head == NULL)
+    {
+        printf("List is empty.\n");
+    }
+    else
+    {
+        temp = head;
+        printf("DATA IN THE LIST:\n");
+ 
+        while(temp != NULL)
+        {
+            printf("DATA of %d node = %d\n", n, temp->data);
+ 
+            n++;
+ 
+            /* Moves the current pointer to next node */
+            temp = temp->next;
+        }
+    }
+}
 int main(){
 
 return(0);
 }
+
